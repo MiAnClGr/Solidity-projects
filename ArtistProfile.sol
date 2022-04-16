6ac54445152f06e69a2921a326d13c8454357b56
MiAnClGr <98620844+MiAnClGr@users.noreply.github.com>
2022-04-16 11:27:34 +1000

    Update ArtistProfile.sol

--- ArtistProfile.sol
+++ ArtistProfile.sol
@@ -2,14 +2,24 @@
 
 pragma solidity >=0.7.0 <0.9.0;
 
+import "./PerformanceContract.sol";
+
 contract ArtistProfile {
 
-    string artistName;
-    address artistAddress;
+    string public artistName;
+    address payable artistAddress;
+
+    PerformanceContract[] public performanceContractArray;
     
     constructor(string memory _artistName) {
         artistName = _artistName;
-        artistAddress = msg.sender;
+        artistAddress = payable(msg.sender);
+    }
+
+    function createPerformanceContract(address venue) public {
+        PerformanceContract performanceContract = new PerformanceContract(artistAddress, venue);
+        performanceContractArray.push(performanceContract);
+
     }
 
     function deposit() public payable {
